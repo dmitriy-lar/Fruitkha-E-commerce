74d1a8781f6f977ca1628388d3579686e2a404c6
dmitriy-lar <larionov.dm2002@gmail.com>
2022-08-19 12:25:32 +0300

    Create shop.html

--- fruitkha/store/urls.py
+++ fruitkha/store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('shop/', views.shop_page, name='shop_page'),
 ]
--- fruitkha/store/views.py
+++ fruitkha/store/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def home_page(request):
     return render(request, 'store/index.html')
 
+
+def shop_page(request):
+    return render(request, 'store/shop.html')

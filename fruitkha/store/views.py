f06c401d6b4e87c40230ec694bfa404b3495a482
dmitriy-lar <larionov.dm2002@gmail.com>
2022-08-20 13:21:43 +0300

    Add paginating system

--- fruitkha/store/views.py
+++ fruitkha/store/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect
 from .forms import NewsLetterForm
 from .models import Category, Product
+
 
 def home_page(request):
     if request.method == 'POST':
@@ -22,10 +24,23 @@ def home_page(request):
 def shop_page(request):
     categories = Category.objects.all()
     products = Product.objects.all()
+
+    paginator = Paginator(products, 1)
+    page_request_value = 'page'
+    page = request.GET.get(page_request_value)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
     context = {
         'categories': categories,
-        'products': products
+        'products': paginated_queryset,
+        'page_request_value': page_request_value
     }
+
     return render(request, 'store/shop.html', context)
 
 

bdf976557d00b224940874807582665bc5eb9106
dmitriy-lar <larionov.dm2002@gmail.com>
2022-08-20 12:43:45 +0300

    Add Product model

--- fruitkha/store/models.py
+++ fruitkha/store/models.py
@@ -17,3 +17,15 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.IntegerField(default=0)
+    thumbnail = models.ImageField(upload_to='thumbnail', null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

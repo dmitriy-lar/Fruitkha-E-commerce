b3b56d2aa4ad693349e9dda2cf0682f5ed2ff761
dmitriy-lar <larionov.dm2002@gmail.com>
2022-08-21 10:36:06 +0300

    Add OrderItem and Order models

--- fruitkha/store/models.py
+++ fruitkha/store/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -31,3 +32,24 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class OrderItem(models.Model):
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} - {self.product.title}"
+
+
+class Order(models.Model):
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.client.username
